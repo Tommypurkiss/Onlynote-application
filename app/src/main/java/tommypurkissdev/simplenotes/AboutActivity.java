6e6c73d8e1c20121354c9e6c3b48786c238feb6a
Tommy <tommypurkiss1@gmail.com>
2018-08-11 16:54:14 +0100

    Initial commit

---
+++ app/src/main/java/tommypurkissdev/simplenotes/AboutActivity.java
@@ -0,0 +1,29 @@
+package tommypurkissdev.simplenotes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import tommypurkissdev.simplenotes.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        TextView textView = findViewById(R.id.about_description);
+    }
+
+
+
+
+
+}
